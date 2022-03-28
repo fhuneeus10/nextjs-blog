32f19bc9b9a39dfdeff977673553fa02ace03052
Federico Huneeus <fahuneeu@uc.cl>
2022-03-28 12:29:00 -0300

    Testing previews on new branch

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Transition leading-edge infrastructures","spans":[]}],"description":[{"type":"paragraph","text":"Mollit dolore in incididunt fugiat.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '../styles/global.css'
+// /pages/_app.js
+
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../prismicio'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>
+            {children}
+          </a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
+}
