305d834aecfbb057f5c579844c970145154af071
Federico Huneeus <fahuneeu@uc.cl>
2022-03-29 17:39:55 -0300

    Adding plugin-nextjs

---
+++ prismicio.js
@@ -0,0 +1,34 @@
+import * as prismic from '@prismicio/client' // client to query content
+import { enableAutoPreviews } from '@prismicio/next' // plugin for previews
+import sm from './sm.json'
+
+export const endpoint = sm.apiEndpoint
+export const repositoryName = prismic.getRepositoryName(endpoint)
+
+/* Update the Link Resolver to match your project's route structure,
+   previews use this to find your docs */
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'homepage':
+      return '/'
+    case 'page':
+      return `/${doc.uid}`
+    default:
+      return null
+  }
+}
+
+// This factory function allows smooth preview setup
+export function createClient(config = {}) {
+  const client = prismic.createClient(endpoint, {
+    ...config,
+  })
+
+  enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req,
+  })
+
+  return client
+}
